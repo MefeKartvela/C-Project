1d5b2bef451330d568bf0e109e59139b4bf60efc
MefeKartvela <you@example.com>
2023-12-18 20:07:54 +0400

    Add project files.

---
+++ C#Project/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Project
+{
+    public partial class Form1 : Form
+    {
+        private const string filePath = "users.txt"; // File to store user credentials
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button_click (object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                if (CreateUser(username, password))
+                {
+                    MessageBox.Show("Signup successful! You can now log in.");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username already exists. Please choose a different username.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter both username and password.");
+            }
+        }
+
+        private bool CreateUser(string username, string password)
+        {
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+
+            
+            string[] existingUsers = File.ReadAllLines(filePath);
+            foreach (string line in existingUsers)
+            {
+                string[] parts = line.Split('|');
+                if (parts.Length > 0 && parts[0] == username)
+                {
+                    return false;
+                }
+            }
+
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine(username + "|" + password);
+            }
+
+            return true; 
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Form2 signupForm = new Form2();
+            signupForm.ShowDialog();
+        }
+    }
+}
---
+++ C#Project/Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Project
+{
+    public partial class Form2 : Form
+    {
+        private const string filePath = "users.txt";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (CheckCredentials(username, password))
+            {
+                MessageBox.Show("Login successful!");
+                
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password!");
+            }
+        }
+
+        private bool CheckCredentials(string username, string password)
+        {
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split('|'); 
+                    if (parts.Length == 2 && parts[0] == username && parts[1] == password)
+                    {
+                        return true; 
+                    }
+                }
+            }
+            return false;
+        }
+
+       
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Form1 signupForm = new Form1();
+            signupForm.ShowDialog();
+        }
+    }
+    }
+
